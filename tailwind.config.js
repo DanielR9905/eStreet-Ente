bfabb7981747c53181105af9241d9465fd8cad91
Daniel Ramirez <danielramirezduque1@gmail.com>
2021-09-04 10:41:48 -0500

    Anexos estilos, página home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 module.exports = {
-  purge: [],
+  purge: [
+      './page/**/*.{js, ts, jsx,, tsx}',
+      './components/**/*.{js, ts, jsx, tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+      extend: {
+          colors: {
+              richBlackFogra: '#0B090A',
+              eerieBlack: '#161A1D',
+              bloodRed: '#660708',
+              rubyRed: '#A4161A',
+              carnelian: '#BA181B',
+              imperialRed: '#E5383B',
+              silverChalice: '#B1A7A6',
+              lightGray: '#D3D3D3',
+              cultured: '#F5F3F4',
+              white: '#FFFFFF',
+          },
+      },
   },
   variants: {
-    extend: {},
+      extend: {
+          backgroundColor: ['checked'],
+          borderColor: ['checked'],
+      },
   },
-  plugins: [],
+  plugins: ['tailwindcss'],
 }
